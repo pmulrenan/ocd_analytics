884f6b8f971fc43be9ea806bd70c33fe715b8e5a
Patrick Mulrenan <pmulrenan20@gmail.com>
2025-01-28 13:29:22 -0500

    Delete ocd_mom_diagnosis.sql

--- ocd_mom_diagnosis.sql
+++
@@ -1,9 +0,0 @@
--- (3) Number of people diagnosed Month over Month
-SELECT
-	COUNT(Patient_ID) AS Count_of_Patients,
-	DATE_FORMAT(OCD_Diagnosis_Date, '%Y-%m-01 00:00:00') as Month_Year
-FROM
-	ocd_patient_dataset
-GROUP BY
-	DATE_FORMAT(OCD_Diagnosis_Date, '%Y-%m-01 00:00:00')
-ORDER BY Month_Year;
